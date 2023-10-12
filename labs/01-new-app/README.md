3e63912de2f067dd8b35c853b02ff4767f376260
Justin Pflueger <justin.pflueger@fermyon.com>
2023-10-12 17:22:26 +0000

    basic devcontainer is working
    Signed-off-by: Justin Pflueger <justin.pflueger@fermyon.com>

---
+++ labs/01-new-app/README.md
@@ -0,0 +1,3 @@
+# Lab 1 - New App
+
+TODO
---
+++ labs/01-new-app/workstation.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+lab_setup()
+{
+    echo "Setting up lab 1..."
+}
+
+lab_check()
+{
+    echo "Setting up lab 1..."
+}
+
+lab_solve()
+{
+    echo "Setting up lab 1..."
+}
